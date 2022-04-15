b544ee456d86272de94414fe1a3ba4351064c9f3
Sarvagya Vinayak <undertaker0432@gmail.com>
2022-04-15 18:32:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+var buttonColours=["red","blue","green","yellow"];
+var gamePattern=[];
+var userClickedPattern = [];
+var started=false;var level=0;
+$(document).keypress(function(){
+if(!started)
+{
+   $("#level-title").text("Level"+level);
+   nextSequence();
+   started=true; 
+}
+});
+$(".btn").click(handler);
+function handler()
+{
+    var userChosenColour= $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour); 
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
+}
+
+function checkAnswer(currentLevel)
+{
+    if(gamePattern[currentLevel]===userClickedPattern[currentLevel])
+    {
+        console.log("Success");
+        if(userClickedPattern.length===gamePattern.length)
+        {
+            setTimeout(function () {
+                nextSequence();
+              }, 1000);
+        }
+    }
+    else{
+        console.log("Wrong");
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+          }, 200);
+          startOver();
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+    }
+}
+function nextSequence()
+{   userClickedPattern=[];
+    level++;
+    $("#level-title").text("Level " + level);
+    var randomNumber= Math.floor(Math.random()*4);
+    var randomChosenColour= buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+$("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+playSound(randomChosenColour);
+
+}
+function playSound(name){
+    var sound= new Audio("sounds/"+name+".mp3");
+    sound.play();
+    
+}
+function animatePress(currentColour){
+$("#"+currentColour).addClass("pressed");
+setTimeout(function(){
+    $("#"+currentColour).removeClass("pressed")},100);
+}
+function startOver()
+{
+    level=0;
+    started=false;
+    gamePattern=[];
+}
